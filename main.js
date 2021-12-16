bb1eed0a65a726be8ff2af21cd4880557c4e0cad
Mega <83192893+MegaCoder2@users.noreply.github.com>
2021-12-15 18:58:50 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,30 @@ function photo()
     })
 }
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/HyjIyzNQx/model.json',modelLoaded);
+function modelLoaded()
+{
+    console.log("Model loaded successfully!")
+}
+function check()
+{
+    img = document.getElementById("image")
+    classifier.classify(img,gotResult)
+}
+function gotResult(error,result)
+{
+    if(error)
+    {
+        console.error("An error occurred here are the details:" + error)
+    }
+    else
+    {
+        console.log(result)
+        document.getElementById("object").innerHTML = result[0].label
+        document.getElementById("accuracy").innerHTML = result[0].confidence.toFixed(3)
+        percent = document.getElementById("accuracy").innerHTML * 255
+        percent2 = percent - 255
+        document.getElementById("object").style.color = "rgb("+percent2+", "+percent+", 0)";
+        document.getElementById("accuracy").style.color = "rgb(0, "+percent+", 0)";
+        console.log(percent)
+    }
+}
